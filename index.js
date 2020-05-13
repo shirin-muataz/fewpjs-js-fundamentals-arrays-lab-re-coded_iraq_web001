301f307768fee853964d79339aeb792ceff01d13
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-13 10:06:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const prepend=['Milo','Otis','Garfield'];
 const removeLast=['Milo','Otis','Garfield'];
 const removeFirst=['Milo','Otis','Garfield'];
 append.push('Odie');
+prepend.unshift('Odie');
+removeLast.pop();
+removeFirst.shift();
